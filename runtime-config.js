4b49c2a70f92eb7470ffa53153ef62d5e2945e6b
Fantasy Time Publisher <publisher@fantasy-time.online>
2026-08-23 06:07:16 +0300

    Publish Fantasy Time snapshot 20260823T030533Z-b0be45b60a71

--- runtime-config.js
+++ runtime-config.js
@@ -3,4 +3,4 @@ document.documentElement.dataset.appMode = window.DOTA_HUB_MODE;
 document.title = "Fantasy Time · TI 2026";
 
 window.DOTA_HUB_STATIC_API = true;
-window.DOTA_HUB_STATIC_RELEASE = "20260823T001031Z-7bb183494362";
+window.DOTA_HUB_STATIC_RELEASE = "20260823T030533Z-b0be45b60a71";
